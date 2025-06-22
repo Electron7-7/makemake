6a240e4dd633b28708583861994cf4bca1e6bb83
Electron77 <teahaile2@teahaile.com>
2025-06-21 21:09:41 -0400

    feature(separated options and flags into their own structs, derived from '_Arg'): Now, I can better isolate 'Option' and 'Flag' command line arguments.

--- src/arguments/argument.hpp
+++ src/arguments/argument.hpp
@@ -2,39 +2,49 @@
 #define ARGUMENT_H
 
 #include "common/compare_strings.hpp"
-#include "common/debugging.hpp"
+#include <string>
 
 struct _Arg
 {
 public:
-    _Arg(const char* LongName, const char* ShortName): _Arg(uids++, LongName, ShortName) {}
-    _Arg(const char* Name): _Arg(-1, Name, Name) {}
-    // _Arg(): _Arg(-1, "", "") {}
-    _Arg(const _Arg& CopyFrom): _Arg(CopyFrom._uid, CopyFrom._long_name, CopyFrom._short_name) {}
+    constexpr _Arg(const char* LongName, const char* ShortName):_long_name(LongName), _short_name(ShortName) {}
+    constexpr _Arg(const _Arg& CopyFrom): _Arg(CopyFrom._long_name, CopyFrom._short_name) {}
+    constexpr _Arg(const char* Name): _Arg(Name, Name) {}
 
-    const char* LongName() const { return _long_name; }
-    const char* ShortName() const { return _short_name; }
-    int UID() const { return _uid; }
+    constexpr std::string LongName()  const { return _long_name;  }
+    constexpr std::string ShortName() const { return _short_name; }
 
-    const bool operator==(const _Arg& other) const
-    {
-        if(_uid > -1 && other._uid > -1)
-            return (_uid == other._uid);
-
-        return (CompareStrings(_long_name, other._long_name) || CompareStrings(_short_name, other._short_name));
-    }
-
-    const bool operator!=(const _Arg& other) const { return !(*this == other);   }
-    const bool operator< (const _Arg& other) const { return (_uid < other._uid); }
-    const bool operator> (const _Arg& other) const { return (_uid > other._uid); }
+    constexpr bool operator==(const _Arg& other) const { return (CompareStrings(_long_name, other._long_name) || CompareStrings(_short_name, other._short_name)); }
+    constexpr bool operator!=(const _Arg& other) const { return !(*this == other);   }
 
 protected:
-    _Arg(int UID, const char* LongName, const char* ShortName): _uid(UID), _long_name(LongName), _short_name(ShortName) {}
-
-    inline static unsigned int uids = 0;
-    int _uid;
-    const char* _long_name  = "";
+    const char* _long_name = "";
     const char* _short_name = "";
+};
+
+struct Flag : public _Arg
+{
+public:
+    using _Arg::_Arg;
+
+    void Activate() { _active = true; }
+    bool IsActive() const { return _active; }
+
+private:
+    bool _active = false;
+};
+
+struct Option : public _Arg
+{
+public:
+    using _Arg::_Arg;
+
+    void SetValue(const char* Value) { _value = Value; }
+    std::string GetValue() const { return _value; }
+    bool HasValue() const { return (!CompareStrings(_value, "")); } // FIXME: Kind of redundant, since I can just check that 'GetValue' isn't an empty string...
+
+private:
+    const char* _value = "";
 };
 
 #endif // ARGUMENT_H
--- src/arguments/arguments.hpp
+++ src/arguments/arguments.hpp
@@ -2,15 +2,28 @@
 #define ARGUMENTS_H
 
 #include "argument.hpp"
+#include <string>
 
 namespace Flags
 {
-    const _Arg Help("--help", "-h");
+    const Flag Help    ("--help", "-h");
+    const Flag Version ("--version", "-v");
 }
 
 namespace Options
 {
-    const _Arg DebugPrint("--debug-print", "-d");
+    const Option DebugPrint       ("--debug-print", "-d");
+    const Option GenerateMakefile ("--makefile", "-m");
 }
+
+inline std::string _Help_Printout =
+R"~(
+    Usage: cpppp [-hv]
+
+        -h, --help      print help document
+        -v, --version   print program version
+)~";
+
+inline std::string _Version_Printout = "0.0.1";
 
 #endif // ARGUMENTS_H
