5fa9fffcbc8ddb36d39ab097f87a8d6d52234b61
Electron7-7 <teahaile2@teahaile.com>
2025-07-22 19:07:37 -0400

    feature(simplified the main function) I'm just using the simple `int main` function for both Linux and Windows.

--- src/system/dedicated_main.cpp
+++ src/system/dedicated_main.cpp
@@ -1,9 +1,8 @@
-#include "common/platform.hpp"
 #include "arguments/arguments.hpp"
 #include "arguments/arguments_parser.hpp"
 #include "argument_handlers.hpp"
 
-int DedicatedMain(int argc, char** argv)
+int main(int argc, char** argv)
 {
     // Add valid flags
     global_ArgumentsParser->AddFlag(Flags::Help);
@@ -25,31 +24,3 @@ int DedicatedMain(int argc, char** argv)
 
     return 0;
 }
-
-// System-specific main functions
-#ifdef PLAT_WINDOWS
-#include <windows.h>
-#include <winbase.h>
-#include <shellapi.h>
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-    int argc,win_return = -1;
-    LPWSTR* argv = CommandLineToArgvW(GetCommandLineW(), &argc);
-
-    win_return = DedicatedMain(argc, (char**)argv);
-    GlobalFree(argv);
-    return win_return;
-}
-#endif // PLAT_WINDOWS
-
-#ifdef PLAT_APPLE
-// Uhhhh, go fuck urself ig? Sorry...
-#endif // PLAT_APPLE
-
-#ifdef PLAT_LINUX
-int main(int argc, char** argv)
-{
-    int linux_return = DedicatedMain(argc, argv);
-    return linux_return;
-}
-#endif // PLAT_LINUX
