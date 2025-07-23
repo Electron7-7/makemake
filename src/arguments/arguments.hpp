7558041ca2d2d1aecca556547ac04a114822ec48
Electron77 <teahaile2@teahaile.com>
2025-07-23 14:33:23 -0400

    feature(updated helpdoc and added a new 'dry run' flag) The dry run flag will only print out the generated Makefile instead of writing it to a file

--- src/arguments/arguments.hpp
+++ src/arguments/arguments.hpp
@@ -5,26 +5,31 @@
 
 namespace Flags
 {
-    const Flag Help    ("--help", "-h");
-    const Flag Version ("--version", "-v");
+    const Flag Help     ("--help", "-h");
+    const Flag Version  ("--version", "-v");
+    const Flag DryRun   ("--dry-run", "-n");
 }
 
 namespace Options
 {
     const Option SourceDirectory ("--source", "-s");
-    const Option ProgramName     ("--name", "-n");
+    const Option ProgramName     ("--name", "-p");
 }
 
 constexpr const char* _Help_Printout =
-R"~(    Usage: cpppp [-hv]
 
-        -h, --help      print help document
-        -v, --version   print program version
+R"~(    Usage: cpppp [-hvn] [--no-color] [-s|--source <source_code_dir>] [-p|--name <program_name>]
+        -h, --help              print help document
+        -v, --version           print program version
+        -n, --dry-run           print the generated Makefile instead of creating/replacing it
+        -s, --source SOURCEDIR  set the source code directory (defaults to 'src')
+        -p, --name NAME         set the name of the final binary (defaults to the name of the current directory)
 )~";
 
 constexpr const char* _Version_Printout = "cpppp v0.0.1";
 
 inline std::string source_directory = "src";
 inline std::string program_name = "";
+inline bool dry_run = false;
 
 #endif // ARGUMENTS_H
