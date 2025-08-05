0bf377a101b20addf096e91282873c2c3fbe6f2c
Electron7-7 <julian@frattarola.net>
2025-08-04 22:51:58 -0400

    feature(improvement): Improved 'try_UpdateSourceDirectories'; the output string now fits perfectly, even if the `SRC_DIRS` lines have been changed. This includes writing all the source directories on the same line, having no whitespace above or below the source directories, not using '\' on the last directory, etc.
    I'm really happy about this :3

--- src/makefile/editing.cpp
+++ src/makefile/editing.cpp
@@ -3,8 +3,10 @@
 #include "generating.hpp"
 #include "data_types.hpp"
 
+#include <print>
 #include <fstream>
 #include <sstream>
+#include <filesystem>
 
 bool try_UpdateSourceDirectories()
 {
@@ -12,7 +14,7 @@ bool try_UpdateSourceDirectories()
 
     // FIXME: Repeated code
     if(replacement_string.ErrorCode() == Err::Generating::SOURCE_DIR_INVALID)
-        return 1;
+        return false;
 
     std::fstream makefile;
     std::stringstream makefile_string_stream;
@@ -21,15 +23,50 @@ bool try_UpdateSourceDirectories()
     makefile.open("Makefile");
     makefile_string_stream << makefile.rdbuf();
     makefile_string = makefile_string_stream.str();
+    makefile.close();
 
     size_t erase_start = makefile_string.find("SRC_DIRS :=");
-    size_t erase_end = makefile_string.rfind("\\\n", makefile_string.find("CC_SRCS")) + 3;
+
+    std::string line_buffer = "";
+
+    size_t iterator = erase_start;
+
+    for(; iterator < makefile_string.size(); iterator++)
+    {
+        char current_character = makefile_string.at(iterator);
+
+        if(current_character != '\n')
+        {
+            line_buffer += current_character;
+            continue;
+        }
+
+        if(line_buffer.contains("$(SRC)/") || line_buffer.starts_with("SRC_DIRS :="))
+        {
+            line_buffer.clear();
+            continue;
+        }
+
+        iterator -= line_buffer.size();
+        break;
+    }
+
+    size_t erase_end = iterator - 1;
+
+    std::string trimmed_replacement_string = replacement_string.Data().GetLine();
+    trimmed_replacement_string.pop_back();
+    if(makefile_string.at(erase_end + 1) == '\n')
+        trimmed_replacement_string.pop_back();
+
+    std::print("Erasing:\n[\x1b[1;41;37m{}\x1b[0m]\n", makefile_string.substr(erase_start, (erase_end - erase_start)));;
+    std::print("Replacement:\n[\x1b[1;42;37m{}\x1b[0m]\n", trimmed_replacement_string);
 
     makefile_string.erase(erase_start, (erase_end - erase_start));
-    makefile_string.insert(erase_start, replacement_string.Data().GetLine());
+    makefile_string.insert(erase_start, trimmed_replacement_string);
 
-    makefile.seekp(0);
+    std::filesystem::remove(std::filesystem::path("Makefile"));
+    makefile.open("Makefile", std::ios_base::out);
     makefile << makefile_string;
     makefile.close();
-    return 0;
+    return true;
 }
