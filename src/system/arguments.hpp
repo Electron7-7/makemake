96e7ee58e2df864ba620ce16e0f025e53ff4490a
Electron7-7 <julian@frattarola.net>
2025-08-12 19:09:06 -0400

    refactor/docs: Improved the 'help' message

--- src/system/arguments.hpp
+++ src/system/arguments.hpp
@@ -22,14 +22,14 @@ namespace Options
 }
 
 constexpr const char* _Help_Printout =
-
-R"~(    Usage: makemake [-hvn] [--no-color] [-s|--source <source_code_dir>] [-p|--name <global_ProgramName>]
+R"~(    Usage: makemake [-h|--help] [-v|--version] [-n|--dry-run] [--no-color]
+                    [-s|--source <directory>] [-p|--name <name>]
         -h, --help                  print help document
         -v, --version               print program version
         -n, --dry-run               print the generated Makefile instead of creating/replacing it
-        -s, --source SOURCEDIR      set the source code directory (defaults to 'src')
+        -s, --source <directory>    specify the root directory for the source code (defaults to './src')
         -u, --update-source-dirs    only update the source directories in an existing Makefile (generates a new Makefile if none exists)
-        -p, --name NAME             set the name of the final binary (defaults to the name of the current directory)
+        -p, --name <name>           set the name of the final binary (defaults to the name of the current directory)
 )~";
 
 constexpr const char* _Version_Printout = "makemake v2.1.2";
