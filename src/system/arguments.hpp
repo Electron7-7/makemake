814a87c13fca37d324ac0664c373e8e4cf17a6c5
Electron7-7 <julian@frattarola.net>
2025-08-14 00:02:19 -0400

    docs: Updated help printout

--- src/system/arguments.hpp
+++ src/system/arguments.hpp
@@ -6,12 +6,12 @@
 namespace Flags
 {
     inline Flag Help             ("--help", "-h");
-    inline Flag Version          ("--version", "-v");
-    inline Flag DryRun           ("--dry-run", "-n");
-    inline Flag NoColor          ("--no-color");
-    inline Flag UpdateSourceDirs ("--update-source-dirs", "-u");
+    inline Flag Version ("--version" , "-v");
+    inline Flag DryRun  ("--dry-run" , "-n");
+    inline Flag NoColor ("--no-color"      );
+    inline Flag Update  ("--update"  , "-u");
 
-    inline Flag debug_NoPrintout ("--no-printout");
+    inline Flag debug_NoPrintout ("--no-printouts");
 }
 
 namespace Options
@@ -23,12 +23,13 @@ namespace Options
 
 constexpr const char* _Help_Printout =
 R"~(    Usage: makemake [-h|--help] [-v|--version] [-n|--dry-run] [--no-color]
-                    [-s|--source <directory>] [-p|--name <name>]
-        -h, --help                  print help document
-        -v, --version               print program version
-        -n, --dry-run               print the generated Makefile instead of creating/replacing it
-        -s, --source <directory>    specify the root directory for the source code (defaults to './src')
-        -u, --update-source-dirs    only update the source directories in an existing Makefile (generates a new Makefile if none exists)
+                    [-s|--source <directory>] [-l|--libraries <directory>] [-p|--name <name>]
+        -h, --help                      print help document
+        -v, --version                   print program version
+        -n, --dry-run                   print finished output instead of writing it to the Makefile
+        -u, --update                    used with -s, -l, -p, etc; updates an existing Makefile, only changing user-specified variables/targets
+        -s, --source <directory>        specify the top-level source code directory (defaults to './src')
+        -l, --libraries <directory>     specify the top-level library directory, e.g: 'src/lib' (no default)
         -p, --name <name>           set the name of the final binary (defaults to the name of the current directory)
 )~";
 
