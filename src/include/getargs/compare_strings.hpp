9d1aef338a0055312d5f679e099098aaed316c27
Electron7-7 <julian@frattarola.net>
2025-08-13 22:53:15 -0400

    refactor: Removed 'common/compare_strings.hpp', as the new version of [getargs](https://github.com/Electron7-7/getargs) will export its own 'compare_strings.hpp' file.
    The `CompareStrings` function *was* still inside one of the header files from `getargs`, but now it's more... obvious.

--- src/include/getargs/compare_strings.hpp
+++ src/include/getargs/compare_strings.hpp
@@ -4,22 +4,22 @@
 constexpr bool CompareStrings(const char* Left, const char* Right)
 {
     if(Left == Right) // Hey, it could happen
-        return true;
+    { return true; }
 
     if(Left == nullptr || Right == nullptr) // All my homies HATE segfaults!!!!
-        return false;
+    { return false; }
 
     while(*Left != '\0' && *Right != '\0')
     {
         if(*Left != *Right)
-            return false;
+        { return false; }
 
         Left++;
         Right++;
     }
 
     if(*Left != '\0' || *Right != '\0') // One of them is shorter than the other!
-        return false;
+    { return false; }
 
     return true;
 }
