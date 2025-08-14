9a858324839cf1a6dbf4268b473c17a1b4fd0c7a
Electron7-7 <julian@frattarola.net>
2025-08-13 23:18:22 -0400

    feature(filesystem.cpp): Created `try_RecursiveGetDirectoriesContainingFileTypes`; a mouthful of a function that simply searches, recursively, for directories containing files of a specified type, via file extension(s), and returns a list of those directories' names. The list is sorted and duplicate entries are removed before returning.

--- src/system/filesystem.cpp
+++ src/system/filesystem.cpp
@@ -4,3 +4,27 @@
 #include <filesystem>
 
 using namespace std::filesystem;
+
+typedef std::list<std::string> DirectoriesList;
+SafeReturn<DirectoriesList> try_RecursiveGetDirectoriesContainingFileTypes(const std::string& start_directory, std::set<std::string> extensions)
+{
+    path start_path = start_directory;
+    DirectoriesList valid_directories = {};
+
+    if(!is_directory(start_path))
+    {
+        // PRINT_ERROR("try_RecursiveGetDirectoriesContainingFileTypes - Starting directory '{}' is invalid", start_directory)
+        return SafeReturn(valid_directories, Status::INVALID_DIRECTORY);
+    }
+
+    for(const auto& entry : recursive_directory_iterator(start_path))
+    {
+        if(entry.is_regular_file() && extensions.contains(entry.path().extension().generic_string()))
+        { valid_directories.push_back(entry.path().parent_path().generic_string()); }
+    }
+
+    valid_directories.sort();
+    valid_directories.unique();
+
+    return SafeReturn(valid_directories);
+}
