935ccce7f815a470f885eadf73bdf3983a8e3314
Electron7-7 <julian@frattarola.net>
2025-08-13 23:38:20 -0400

    feature+chore: Removed prototype code for finding library files/directories, and changed how default 'Option' values are set.

--- src/system/dedicated_main.cpp
+++ src/system/dedicated_main.cpp
@@ -1,57 +1,17 @@
 #include "arguments.hpp"
 #include "getargs/argument_parser.hpp"
 #include "common/printing.hpp"
-#include "makefile/default_makefile.hpp"
-#include "makefile/generating.hpp"
-#include "makefile/editing.hpp"
+#include "makefile/interfacing.hpp"
 
 #include <print>
 #include <filesystem>
 #include <fstream>
-#include <array>
-#include <list>
 
-constexpr const char* ERR_STR_SOURCE_DIR_INVALID = "The provided source directory is invalid/doesn't exist!";
 constexpr const char* default_SourceCodeDirectory = "src";
+constexpr const char* default_LibrariesDirectory  = "src/lib";
 const std::string     default_ProgramName = std::filesystem::current_path().stem().string();
 
-struct FileDir
-{
-    std::string filename = "";
-    std::string directory = "";
-};
-
-std::array<std::string, 3> strings_Windows =
-{
-    "windows",
-    "Windows",
-    "WINDOWS",
-};
-
-std::array<std::string, 3> strings_Linux =
-{
-    "linux",
-    "Linux",
-    "LINUX",
-};
-
-enum class DirStatus
-{
-    LINUX    = 0b00,
-    WINDOWS  = 0b01,
-    AGNOSTIC = 0b10,
-};
-
-DirStatus CheckLibraryDir(const std::string& directory)
-{
-    for(std::string& string : strings_Windows)
-    { if(directory.contains(string)) { return DirStatus::WINDOWS; } }
-
-    for(std::string& string : strings_Linux)
-        { if(directory.contains(string)) { return DirStatus::LINUX; } }
-
-    return DirStatus::AGNOSTIC;
-}
+const char* constant_MakefileFileName = "Makefile";
 
 int main(int argc, char** argv)
 {
@@ -85,10 +45,10 @@ int main(int argc, char** argv)
     if(Flags::debug_NoPrintout.IsActive())
     { Flags::DryRun.Activate(); }
 
-    if(!Options::SourceDirectory.IsActive() || !Options::SourceDirectory.HasValue())
+    if(!Options::SourceDirectory.HasValue())
     { Options::SourceDirectory.SetValue(default_SourceCodeDirectory); }
 
-    if(!Options::ProgramName.IsActive() || !Options::ProgramName.HasValue())
+    if(!Options::ProgramName.HasValue())
     { Options::ProgramName.SetValue(default_ProgramName.c_str()); }
 
     if(Flags::UpdateSourceDirs.IsActive() && std::filesystem::exists(std::filesystem::path("Makefile")))
@@ -101,104 +61,12 @@ int main(int argc, char** argv)
         }
         return 0;
     }
-
-    if(Options::Libraries.IsActive() && Options::Libraries.HasValue())
-    {
-        const char* library_path = Options::Libraries.GetValue();
-        if(!std::filesystem::exists(std::filesystem::path(library_path)))
-        { PRINT_WARNING("Specified library directory '{}' doesn't exist. The defeault Makefile will still be generated.", library_path) }
-        else
-        {
-            std::list<std::string> linux_libraries = {};
-            std::list<std::string> windows_libraries = {};
-
-            std::list<std::string> linux_lib_subdirs = {};
-            std::list<std::string> windows_lib_subdirs = {};
-
-            std::string current_library = "";
-            std::string current_lib_subdir = Options::Libraries.GetValue();
-
-            std::filesystem::path library_path = current_lib_subdir;
-            DirStatus library_subdir_status = DirStatus::AGNOSTIC;
-
-            for(const auto& entry : std::filesystem::recursive_directory_iterator(library_path))
-            {
-                std::string path_name = entry.path().generic_string();
-
-                if(entry.is_directory())
-                {
-                    library_subdir_status = CheckLibraryDir(path_name);
-                    current_lib_subdir = path_name;
-                }
-
-                if(entry.is_regular_file())
-                {
-                    if(entry.path().parent_path() == library_path)
-                    {
-                        library_subdir_status = DirStatus::AGNOSTIC;
-                        current_lib_subdir = library_path.generic_string();
-                    }
-
-                    current_library = entry.path().stem().generic_string().erase(0, 3); // library name minus the extension and "lib" (so, "libgetargs.a" becomes "getargs")
-
-                    // FIXME: There's got to be a better way of doing this...
-                    switch(library_subdir_status)
-                    {
-                    case DirStatus::AGNOSTIC:
-                        break;
-                    case DirStatus::WINDOWS:
-                        windows_libraries.push_back(current_library);
-                        windows_lib_subdirs.push_back(current_lib_subdir);
-                        continue;
-                    case DirStatus::LINUX:
-                        linux_libraries.push_back(current_library);
-                        linux_lib_subdirs.push_back(current_lib_subdir);
-                        continue;
-                    }
-
-                    if(!entry.path().extension().generic_string().compare(".dll")) // Just in case
-                    {
-                        windows_libraries.push_back(current_library);
-                        windows_lib_subdirs.push_back(current_lib_subdir);
-                        continue;
-                    }
-
-                    windows_libraries.push_back(current_library);
-                    windows_lib_subdirs.push_back(current_lib_subdir);
-                    linux_libraries.push_back(current_library);
-                    linux_lib_subdirs.push_back(current_lib_subdir);
-                }
             }
 
-            if(!linux_libraries.empty())
+    if(!Options::Libraries.HasValue())
+    { Options::Libraries.SetValue(default_LibrariesDirectory); }
+
             {
-                std::string new_value = MakeVariables::LDFLAGS_LINUX.ClearValue();
-
-                for(const std::string& directory: linux_lib_subdirs)
-                { new_value.append(" -L" + directory); }
-
-                for(const std::string& library: linux_libraries)
-                { new_value.append(" -l" + library); }
-
-                new_value.pop_back();
-
-                MakeVariables::LDFLAGS_LINUX.SetValue(new_value);
-                PRINT_DEBUG("LDFLAGS_LINUX: '{}'", new_value)
-            }
-
-            if(!windows_libraries.empty())
-            {
-                std::string new_value = MakeVariables::LDFLAGS_WINDOWS.ClearValue();
-
-                for(const std::string& directory: windows_lib_subdirs)
-                { new_value.append(" -L" + directory); }
-
-                for(const std::string& library: windows_libraries)
-                { new_value.append(" -l" + library); }
-
-                MakeVariables::LDFLAGS_WINDOWS.SetValue(new_value);
-                PRINT_DEBUG("LDFLAGS_WINDOWS: '{}'", new_value)
-            }
         }
     }
 
